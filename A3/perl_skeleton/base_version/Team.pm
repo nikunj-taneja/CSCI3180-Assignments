3f7d152701d945e9cc8729fdb2e0b9d33d4e184d
nikunj-taneja <niktaneja99@gmail.com>
2022-04-08 21:40:46 +0530

    add Team.pm implementation

--- A3/perl_skeleton/base_version/Team.pm
+++ A3/perl_skeleton/base_version/Team.pm
@@ -1,17 +1,56 @@
 use strict;
 use warnings;
 
-package Team;
+use lib "/Users/nikunj/Desktop/CSCI3180/CSCI3180-Assignments/A3/perl_skeleton/";
+use base_version::Fighter;
 
-sub new {};
+package Team;
 
-sub set_fighter_list{
-}
-sub get_fighter_list{
+sub new {
+    my $class = shift;
+    my $self = {
+        "NO" => shift,
+        "fighter_list" => undef,
+        "order" => undef,
+        "fight_cnt" => shift
+    };
+    return bless $self, $class;
+};
+
+sub set_fighter_list {
+    my ( $self, $fighter_list ) = @_;
+    $self->{"fighter_list"} = $fighter_list;
 }
 
-sub set_order{
+sub get_fighter_list {
+    my ( $self ) = @_;
+    return $self->{"fighter_list"};
 }
-sub get_next_fighter{
+
+sub set_order {
+    my ( $self, $order ) = @_;
+    for my $a_order (@$order) {
+        push(@{$self->{"order"}}, $a_order);
+    }
+    $self->{"fight_cnt"} = 0;
 }
-1;
+
+sub get_next_fighter {
+    my ( $self ) = @_;
+    if ($self->{"fight_cnt"} >= scalar @{$self->{"order"}}) {
+        return undef;
+    }
+    my $prev_fighter_idx = $$self->{"order"}[$self->{"fight_cnt"}];
+    my $fighter = undef;
+    for my $cur_fighter (@$self->{"fighter_list"}) {
+        if (${$cur_fighter->get_properties()}{"NO"} == $prev_fighter_idx) {
+            $fighter = $$cur_fighter;
+            last;
+        }
+    }
+    $self->{"fighter_cnt"} += 1;
+    return $fighter;
+}
+
+
+1;
