753cb140f69f879b42e63d95af96b752532ce0ee
nikunj-taneja <niktaneja99@gmail.com>
2022-04-08 15:49:39 +0530

    add Fighter.pm implementation

--- A3/perl_skeleton/base_version/Fighter.pm
+++ A3/perl_skeleton/base_version/Fighter.pm
@@ -3,26 +3,54 @@ use warnings;
 
 package Fighter;
 
+sub new {
+  my $class = shift;
+  my $self = {
+    "NO" => shift,
+    "HP" => shift,
+    "attack" => shift,
+    "defense" => shift,
+    "speed" => shift,
+    "defeated" => shift
+  };
 
-sub new{
+  return bless $self, $class;
 }
 
-sub get_properties{
+sub get_properties {
+  my ( $self ) = @_;
+  return {
+    "NO" => $self->{"NO"},
+    "HP" => $self->{"HP"},
+    "attack" => $self->{"attack"},
+    "defense" => $self->{"defense"},
+    "speed" => $self->{"speed"},
+    "defeated" => $self->{"defeated"}
+  };
 }
 
-sub reduce_HP{
+sub reduce_HP {
+  my ( $self, $damage ) = @_;
+  $self->{"HP"} -= $damage;
+  if ($self->{"HP"} <= 0) {
+    $self->{"HP"} = 0;
+    $self->{"defeated"} = 1;
+  }
+  return;
 }
 
-sub check_defeated{
+sub check_defeated {
+  my ( $self ) = @_;
+  return $self->{"defeated"};
 }
 
-sub print_info{
+sub print_info {
   my $self = shift;
-  
+
   my $defeated_info;
-  if ($self -> check_defeated() == 1){
+  if ($self -> check_defeated() == 1) {
     $defeated_info = "defeated";
-  }else{
+  } else {
     $defeated_info = "undefeated";
   }
   print "Fighter $self->{'NO'}: HP: $self->{'HP'} attack: $self->{'attack'} defense: $self->{'defense'} speed: $self->{'speed'} $defeated_info\n";
