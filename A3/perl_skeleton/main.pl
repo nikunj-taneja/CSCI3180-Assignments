f38fe3f445d38d2d1343823a93361707bd042bcd
nikunj-taneja <niktaneja99@gmail.com>
2022-04-10 19:38:09 +0530

    test advanced version for perl

--- A3/perl_skeleton/main.pl
+++ A3/perl_skeleton/main.pl
@@ -2,13 +2,12 @@ use strict;
 use warnings;
 
 # switch version here
-
 use lib "./";
 
-use base_version::Tournament;
-my $tournament = new Tournament();
+# use base_version::Tournament;
+# my $tournament = new Tournament();
 
-# use advanced_version::AdvancedTournament;
-# my $tournament = new AdvancedTournament();
+use advanced_version::AdvancedTournament;
+my $tournament = new AdvancedTournament();
 
 $tournament -> play_game();
