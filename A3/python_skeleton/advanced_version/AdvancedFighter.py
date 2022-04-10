2298f399336d9a5e010a96ffc91f1cd93ac20ee3
nikunj-taneja <niktaneja99@gmail.com>
2022-04-10 19:36:35 +0530

    add python advanced version implementation

--- A3/python_skeleton/advanced_version/AdvancedFighter.py
+++ A3/python_skeleton/advanced_version/AdvancedFighter.py
@@ -13,13 +13,34 @@ class AdvancedFighter(Fighter):
         self.history_record = []
 
     def obtain_coins(self):
-        ...
+        self.coins += coins_to_obtain
         
     def buy_prop_upgrade(self):
-        ...
+        while self.coins >= 50:
+            strat = input(
+                f"Do you want to upgrade properties for Fighter {self.NO}? "\
+                "A for attack. D for defense. S for speed. N for no.\n"
+            )
+            if strat == 'N':
+                return
+            
+            self.coins -= 50
+            if strat == 'A':
+                self.attack += 1
+            elif strat == 'D':
+                self.defense += 1
+            else:
+                self.speed += 1
         
     def update_properties(self):
-        ...
+        self.attack = max(self.attack + delta_attack, 1)
+        self.defense = max(self.defense + delta_defense, 1)
+        self.speed = max(self.speed + delta_speed, 1)
 
     def record_fight(self, fight_result):
-        ...
+        self.history_record.append(fight_result)
+        
+        # maintain record for only recent three duels
+        if len(self.history_record) > 3:
+            self.history_record.pop(0)
+        
